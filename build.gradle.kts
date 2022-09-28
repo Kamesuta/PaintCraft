bdbb97dc346c615ccf4ec4dde001191fc9374c35
Kamesuta <kamesuta@gmail.com>
2022-09-29 01:51:27 +0900

    Multiplatform導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,10 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.ajoberstar.grgit.Grgit
 
 plugins {
+    // Javaプラグインを適用
+    java
     // Kotlinを使用するためのプラグイン
-    kotlin("jvm") version "1.7.10"
+    kotlin("multiplatform") version "1.7.10"
     // Kdoc(Kotlin用Javadoc)を生成するためのプラグイン
     id("org.jetbrains.dokka") version "1.7.10"
     // ShadowJar(依存関係埋め込み)を使用するためのプラグイン
@@ -45,43 +47,47 @@ repositories {
     maven("https://repo.dmulloy2.net/repository/public/")
 }
 
-sourceSets {
-    val common by creating
-    val main by getting {
-        // 共通ソースを追加
-        compileClasspath += common.output
-        runtimeClasspath += common.output
-
-        dependencies {
-            // PaperAPI
-            compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-            // ProtocolLib
-            compileOnly("com.comphenix.protocol:ProtocolLib:4.6.0")
-            // FlyLib (コマンド用, 廃止予定)
-            implementation("dev.kotx:flylib-reloaded:0.5.0")
+kotlin {
+    // Bukkit使用部分と非使用部分を分離する
+    jvm("bukkit") {
+        // Java8互換
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
         }
     }
-}
 
-java {
-    // ソースjarを生成する
-    withSourcesJar()
-
-    // Java8でコンパイルする
-    val targetJavaVersion = 8
-    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
-    sourceCompatibility = javaVersion
-    targetCompatibility = javaVersion
-
-    // ビルドツールチェーンがコンパイルするJavaより古い場合は、コンパイルするJavaを使用する
-    if (JavaVersion.current() < javaVersion) {
-        val javaLanguageVersion = JavaLanguageVersion.of(targetJavaVersion)
-        toolchain.languageVersion.set(javaLanguageVersion)
+    // Bukkit部分のみ依存関係を追加する
+    sourceSets {
+        val commonMain by getting
+        val bukkitMain by getting {
+            dependencies {
+                // PaperAPI
+                compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+                // ProtocolLib
+                compileOnly("com.comphenix.protocol:ProtocolLib:4.6.0")
+                // FlyLib (コマンド用, 廃止予定)
+                implementation("dev.kotx:flylib-reloaded:0.5.0")
+            }
+        }
     }
 }
 
 tasks {
     jar {
+        // デフォルトのjarタスクを削除
+        enabled = false
+    }
+
+    val allMetadataJar by getting(Jar::class) {
+        // -metadata.jarを生成するタスクを削除
+        enabled = false
+    }
+
+    val bukkitJar by getting(Jar::class) {
+        // -bukkitを除く
+        archiveAppendix.set("")
         // 依存関係を埋め込んでいないjarは末尾に-originalを付与する
         archiveClassifier.set("original")
     }
@@ -92,8 +98,10 @@ tasks {
         prefix = "${project.group}.${project.name}.lib"
     }
 
+    // デバッグ用のfatJarを生成する (リロケートあり版)
     shadowJar {
-        dependsOn(jar)
+        // jarタスクの出力を依存関係に追加
+        dependsOn(bukkitJar)
         // リロケートする
         dependsOn(relocateShadow)
         // 依存関係を埋め込んだjarは末尾なし
@@ -103,42 +111,58 @@ tasks {
     // デバッグ用のfatJarを生成する (ホットリロードできるようにリロケートを行わない)
     val shadowJarDev by registering(ShadowJar::class) {
         // jarタスクの出力を依存関係に追加
-        dependsOn(jar)
-        // 必要な変数を取得
-        val common by sourceSets.getting
-        val main by sourceSets.getting
-        val runtimeClasspath by project.configurations.getting
-        // ShadowJarタスクを新規作成する際の設定を行う (ShadowJavaPlugin.groovyを参照)
-        // https://github.com/johnrengelman/shadow/issues/108#issuecomment-62418005
-        from(common.output)
-        from(main.output)
-        configurations = listOf(runtimeClasspath)
-        exclude("META-INF/INDEX.LIST", "META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA", "module-info.class")
+        dependsOn(bukkitJar)
         // デバッグ用は末尾に-devを付与する
         archiveClassifier.set("dev")
     }
 
-    build {
-        // 依存関係を埋め込んだjarをビルドする
-        dependsOn(shadowJar)
-        // 依存関係を埋め込んだjarをビルドする (リロケートなし版)
-        dependsOn(shadowJarDev)
+    // ShadowJarタスクを新規作成する際の設定を行う (ShadowJavaPlugin.groovyを参照)
+    // https://github.com/johnrengelman/shadow/issues/108#issuecomment-62418005
+    withType<ShadowJar> {
+        // 必要な変数を取得
+        val bukkit by kotlin.targets.getting
+        val main by bukkit.compilations.getting
+        val bukkitRuntimeClasspath by project.configurations.getting
+        // ShadowJarタスクを新規作成する際の設定を行う (ShadowJavaPlugin.groovyを参照)
+        // https://github.com/johnrengelman/shadow/issues/108#issuecomment-62418005
+        from(main.output)
+        configurations = listOf(bukkitRuntimeClasspath)
+        exclude("META-INF/INDEX.LIST", "META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA", "module-info.class")
     }
 
-    // Kdocをjavadoc.jarに出力する
-    val javadocJar by creating(Jar::class) {
+    // Javadocを出力する
+    val javadocJar by registering(Jar::class) {
         archiveClassifier.set("javadoc")
         from(dokkaJavadoc)
         dependsOn(dokkaJavadoc)
     }
 
+    // ソースjarを生成する
+    val sourcesJar by registering(Jar::class) {
+        archiveClassifier.set("sources")
+        val commonMain by kotlin.sourceSets.getting
+        val bukkitMain by kotlin.sourceSets.getting
+        from(commonMain.kotlin.srcDirs)
+        from(bukkitMain.kotlin.srcDirs)
+    }
+
+    // アーティファクトを登録する
     artifacts {
-        // javadoc.jarをアーティファクトとして登録する
+        // 依存関係なしオリジナルjarをビルドする (-original)
+        add("archives", bukkitJar)
+        // 依存関係を埋め込んだリロケート済みjarをビルドする
+        add("archives", shadowJar)
+        // 依存関係を埋め込んだリロケートなしjarをビルドする (-dev)
+        add("archives", shadowJarDev)
+        // Javadocを出力する (-javadoc)
         add("archives", javadocJar)
+        // ソースjarを生成する (-sources)
+        add("archives", sourcesJar)
     }
 
     // plugin.ymlの中にバージョンを埋め込む
-    processResources {
+    @Suppress("UnstableApiUsage")
+    withType<ProcessResources> {
         val props = mapOf("version" to version)
         inputs.properties(props)
         filteringCharset = "UTF-8"
